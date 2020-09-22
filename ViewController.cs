251d447df316146f8fcba91ccdd9d26809b421fa
anjaeo <anjaeo93@gmail.com>
2020-09-22 11:35:13 -0400

    Initial check-in of module Calculator

---
+++ ViewController.cs
@@ -0,0 +1,188 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace test1
+{
+    public partial class ViewController : UIViewController
+    {
+        string input = string.Empty;
+        string operand1 = string.Empty;
+        string operand2 = string.Empty;
+        char operation;
+        double result = 0.0;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void Zero_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "0";
+            this.display.Text += input;
+        }
+
+        partial void One_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "1";
+            this.display.Text += input;
+        }
+
+        partial void Two_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "2";
+            this.display.Text += input;
+        }
+
+        partial void Three_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "3";
+            this.display.Text += input;
+        }
+
+        partial void Four_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "4";
+            this.display.Text += input;
+        }
+
+        partial void Five_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "5";
+            this.display.Text += input;
+        }
+
+        partial void Six_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "6";
+            this.display.Text += input;
+        }
+
+        partial void Seven_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "7";
+            this.display.Text += input;
+        }
+
+        partial void Eight_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "8";
+            this.display.Text += input;
+        }
+
+        partial void Nine_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += "9";
+            this.display.Text += input;
+        }
+
+        partial void DecimalBtn_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "";
+            input += ".";
+            this.display.Text += input;
+        }
+
+        partial void ClearBtn_TouchUpInside(UIButton sender)
+        {
+            this.display.Text = "0";
+            this.input = string.Empty;
+            this.operand1 = string.Empty;
+            this.operand2 = string.Empty;
+        }
+
+        partial void DivideBtn_TouchUpInside(UIButton sender)
+        {
+            operand1 = input;
+            operation = '/';
+            input = string.Empty;
+        }
+
+        partial void MultiplyBtn_TouchUpInside(UIButton sender)
+        {
+            operand1 = input;
+            operation = '*';
+            input = string.Empty;
+        }
+
+        partial void SubtractBtn_TouchUpInside(UIButton sender)
+        {
+            operand1 = input;
+            operation = '-';
+            input = string.Empty;
+        }
+
+        partial void AddBtn_TouchUpInside(UIButton sender)
+        {
+            operand1 = input;
+            operation = '+';
+            input = string.Empty;
+        }
+
+        partial void EqualsBtn_TouchUpInside(UIButton sender)
+        {
+            operand2 = input;
+            double num1, num2;
+            double.TryParse(operand1, out num1);
+            double.TryParse(operand2, out num2);
+
+            this.display.Text = "0";
+            this.input = string.Empty;
+            this.operand1 = string.Empty;
+            this.operand2 = string.Empty;
+
+            if (operation == '+')
+            {
+                result = num1 + num2;
+                display.Text = result.ToString();
+            }
+            else if (operation == '-')
+            {
+                result = num1 - num2;
+                display.Text = result.ToString();
+            }
+            else if (operation == '*')
+            {
+                result = num1 * num2;
+                display.Text = result.ToString();
+            }
+            else if (operation == '/')
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    display.Text = result.ToString();
+                }
+                else
+                {
+                    display.Text = "DIV/Zero!";
+                }
+
+            }
+        }
+
+        
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,259 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace test1
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton addBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton clearBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton decimalBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel display { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton divideBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton eight { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton equalsBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton five { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton four { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton multiplyBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton nine { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton one { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton seven { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton six { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton subtractBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton three { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel titleLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton two { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton zero { get; set; }
+
+        [Action ("AddBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void AddBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("ClearBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void ClearBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("DecimalBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void DecimalBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("DivideBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void DivideBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Eight_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Eight_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("EqualsBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void EqualsBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Five_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Five_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Four_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Four_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("MultiplyBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void MultiplyBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Nine_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Nine_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("One_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void One_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Seven_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Seven_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Six_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Six_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("SubtractBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void SubtractBtn_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Three_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Three_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Two_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Two_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Zero_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Zero_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (addBtn != null) {
+                addBtn.Dispose ();
+                addBtn = null;
+            }
+
+            if (clearBtn != null) {
+                clearBtn.Dispose ();
+                clearBtn = null;
+            }
+
+            if (decimalBtn != null) {
+                decimalBtn.Dispose ();
+                decimalBtn = null;
+            }
+
+            if (display != null) {
+                display.Dispose ();
+                display = null;
+            }
+
+            if (divideBtn != null) {
+                divideBtn.Dispose ();
+                divideBtn = null;
+            }
+
+            if (eight != null) {
+                eight.Dispose ();
+                eight = null;
+            }
+
+            if (equalsBtn != null) {
+                equalsBtn.Dispose ();
+                equalsBtn = null;
+            }
+
+            if (five != null) {
+                five.Dispose ();
+                five = null;
+            }
+
+            if (four != null) {
+                four.Dispose ();
+                four = null;
+            }
+
+            if (multiplyBtn != null) {
+                multiplyBtn.Dispose ();
+                multiplyBtn = null;
+            }
+
+            if (nine != null) {
+                nine.Dispose ();
+                nine = null;
+            }
+
+            if (one != null) {
+                one.Dispose ();
+                one = null;
+            }
+
+            if (seven != null) {
+                seven.Dispose ();
+                seven = null;
+            }
+
+            if (six != null) {
+                six.Dispose ();
+                six = null;
+            }
+
+            if (subtractBtn != null) {
+                subtractBtn.Dispose ();
+                subtractBtn = null;
+            }
+
+            if (three != null) {
+                three.Dispose ();
+                three = null;
+            }
+
+            if (titleLabel != null) {
+                titleLabel.Dispose ();
+                titleLabel = null;
+            }
+
+            if (two != null) {
+                two.Dispose ();
+                two = null;
+            }
+
+            if (zero != null) {
+                zero.Dispose ();
+                zero = null;
+            }
+        }
+    }
+}
